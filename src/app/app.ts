eb138dfb1da83a3c4d26e3d2350edbf9eb463e36
Marienoir <okosunmaryeghonghon@gmail.com>
2025-10-23 14:03:17 +0100

    update to latest dependencies

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-i18n-localazy-demo');
+  protected readonly minutes = signal(0);
+  protected readonly user = signal('localazy');
+}
